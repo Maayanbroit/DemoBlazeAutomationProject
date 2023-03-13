a7961c23e61d142e37f18901d673e5944da28ec5
shiranohayon <127047832+shiranohayon@users.noreply.github.com>
2023-03-13 14:27:58 +0200

    shiran tests

--- utilities/chromedriver.py
+++ utilities/chromedriver.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service
 def get_chrome_driver():
     """please, pay attention to the chrome driver"""
     #chromedriver_path = Fill in windows chrome drivers
-    chromedriver_path = "/Users/maayan/Downloads/Wed Drivers/chromedriver"
+    chromedriver_path = 'C:/chromedriver/chromedriver.exe'
     service = Service(executable_path=chromedriver_path)
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-extensions")
